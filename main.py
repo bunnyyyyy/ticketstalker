273bc71eaeaa3a76b9c62f599c38014705972c9d
bunnyyy <sanya.badhe@gmail.com>
2023-06-13 13:40:52 -0700

    undetected chrome driver works- still blocked

--- main.py
+++
@@ -1,4 +0,0 @@
-import webbot
-
-
-print("hey")
---
+++ scraper.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+
+import random
+import time
+
+
+driver = uc.Chrome()
+
+driver.get("https://seatgeek.com/taylor-swift-with-haim-and-gracie-abrams-tickets/santa-clara-california-levi-s-stadium-2023-07-28-6-30-pm/concert/5862048")
+
+time.sleep(10)
+
+#driver.get("https://nowsecure.nl")
+
+# try:
+#     desired_element = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#__next .MappedEventLayout__MapPage-sc-c5b97ed1-0.hOHnAR .MapAndListingsLayout__DesktopWrapper-sc-2188243b-0.iXTtTd .Omnibox__PanelWrapper-sc-2ba63abd-1.eZDNqq .presenters__ViewColumn-sc-de03606d-0.dISTYK [data-test="all-listings"] [aria-label*=" ticket"]')))
+#     print("worked")
+
+# except TimeoutError:
+#     print("element not here")
+
+# time.sleep(5)
+#desired_element = driver.find_element(By.CSS_SELECTOR, '#__next .MappedEventLayout__MapPage-sc-c5b97ed1-0.hOHnAR .MapAndListingsLayout__DesktopWrapper-sc-2188243b-0.iXTtTd')
+
+
+# start-of-content > div.Omnibox__PanelWrapper-sc-2ba63abd-1.eZDNqq > div.presenters__ViewColumn-sc-de03606d-0.dISTYK > div > div:nth-child(5) > div
+# 
+#  #__next .MappedEventLayout__MapPage-sc-c5b97ed1-0.hOHnAR .MapAndListingsLayout__DesktopWrapper-sc-2188243b-0.iXTtTd .Omnibox__PanelWrapper-sc-2ba63abd-1.eZDNqq .presenters__ViewColumn-sc-de03606d-0.dISTYK [data-test="all-listings"] [aria-label*="ticket at"]
+
+# text = desired_element.get_attribute('aria-label')
+# print(text)
+
+# driver.quit()
